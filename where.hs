662a903a8324f4fae56a772623eef93a71f9d22b
Philip Castiglione <philipcastiglione@gmail.com>
2016-11-14 17:10:27 +1100

    book ch2 exercises

---
+++ where.hs
@@ -0,0 +1,16 @@
+mult1     = x * y
+  where x = 5
+        y = 6
+
+thing1 = x * 3 + y
+  where x = 3
+        y = 1000
+
+thing2 = x * 5
+  where y = 10
+        x = 10 * 5 + y
+
+thing3 = z / x + y
+  where x = 7
+        y = negate x
+        z = y * 10
